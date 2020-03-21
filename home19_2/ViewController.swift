cc50b092a2fabdba771328135919d8907d9b14de
Vidrul <Vidrul@ugithub.com>
2020-03-21 11:22:49 +0300

    Initial Commit

---
+++ home19_2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  home19_2
+//
+//  Created by David Saley on 3/21/20.
+//  Copyright © 2020 David. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
